7bff821f56ae3beebd88c12a64085fe265db0ca6
Andy Lulham <a.lulham@gmail.com>
2021-04-01 04:17:17 +0100

    Maintain labels via a github action

---
+++ update_publisher_labels.py
@@ -0,0 +1,42 @@
+from os import getenv
+
+import requests
+from github import Github
+
+
+pub_label_prefix = 'publisher: '
+pub_labels = []
+j = requests.get("https://iatiregistry.org/publisher/download/json").json()
+for x in j:
+    desc = "Issue relates to " + x["Publisher"]
+    if len(desc) > 100:
+        desc = desc[:97] + "[…]"
+    publisher_id = x["Datasets Link"].rsplit("/", 1)[-1]
+    name = pub_label_prefix + publisher_id
+    pub_labels.append({"name": name, "description": desc, "color": "ededed"})
+
+pub_labels = {label["name"]: label for label in pub_labels}
+
+g = Github(getenv('GITHUB_TOKEN'))
+repo = g.get_repo('codeforiati/iati-data-bugtracker')
+repo_labels = list(repo.get_labels())
+for repo_label in repo_labels:
+    if not repo_label.name.startswith(pub_label_prefix):
+        continue
+    current_label = pub_labels.get(repo_label.name)
+    if not current_label:
+        print('Deleting label: "{}"'.format(repo_label.name))
+        repo_label.delete()
+        continue
+    new_description = current_label.get('description', repo_label.description)
+    new_color = current_label.get('color', repo_label.color)
+    if new_description != repo_label.description or \
+            new_color != repo_label.color:
+        print('Updating label: "{}"'.format(repo_label.name))
+        repo_label.edit(repo_label.name, new_color, new_description)
+
+repo_label_names = [x.name for x in repo_labels]
+for label_name, label_dict in pub_labels.items():
+    if label_name not in repo_label_names:
+        print('Creating label: {}'.format(label_name))
+        repo.create_label(**label_dict)
