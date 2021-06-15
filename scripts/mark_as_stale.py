b1a8b4bd1588d77b5950940cb335c3893c1630a1
Andy Lulham <a.lulham@gmail.com>
2021-06-15 23:58:30 +0100

    Scrap stale bot

---
+++ scripts/mark_as_stale.py
@@ -0,0 +1,33 @@
+from datetime import date, timedelta
+from os import getenv
+
+from github import Github
+
+days_until_stale = 30
+today = date.today()
+stale_before = today - timedelta(days=days_until_stale)
+exempt_labels = ['meta', 'evergreen']
+
+g = Github(getenv('GITHUB_TOKEN'))
+repo = g.get_repo(getenv('GITHUB_REPOSITORY'))
+
+issues = repo.get_issues(sort='updated', direction='asc')
+for issue in issues:
+    issue_labels = [l.name for l in issue.labels]
+    if any([label in issue_labels for label in exempt_labels]):
+        continue
+    if issue.updated_at.date() <= stale_before:
+        # issue is stale
+        issue.add_to_labels('awaiting update')
+        update_message = """Hello! There has been no activity on this issue in the last 30 days. I wonder if it has now been resolved?
+
+If you’re reading this, would you mind checking to see if the issue is still applicable?
+
+* If it has been resolved, please add a message to that effect, and (if you’re able to) close it.
+* If the problem still applies, please add a message to let us know. I’ll remove the "awaiting update" label, and will check in again next month.
+
+Thank you!"""
+        issue.create_comment(update_message)
+    else:
+        if 'awaiting update' in issue_labels:
+            issue.remove_from_labels('awaiting update')
